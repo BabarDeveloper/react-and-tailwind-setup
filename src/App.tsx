1d4411e463dc4507a69dd26364da0068df5d7dd1
BabarDeveloper <babardeveloper2@gmail.com>
2026-05-01 02:20:26 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button variant="default">Default</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="link">Link</Button>
+    </>
+  );
+}
+
+export default App;
